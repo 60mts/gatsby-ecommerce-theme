5d31c4cb42fb5af8bfe5aeb93db01e2dfeb870f0
ᴍᴜʜᴀᴍᴍᴇᴅ ᴛᴀʏʏɪʙ Şᴀʜɪɴ <muhammedtayyibsahin@yandex.com>
2022-06-11 23:46:37 +0300

    fff

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -12,7 +12,7 @@ const Brand = (props) => {
     >
       {/* <h4>SYDNEY</h4> */}
     
-        <img src={'../static/ata-logo.jpg'}/>
+        <img src={'../static/atamuh.jpg'}/>
 
     </div>
   );
